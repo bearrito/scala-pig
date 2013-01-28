446fd58e8d15bce358c5d041f7b788bec2364826
barrett <j.barrett.strausser@gmail.com>
2013-01-27 21:32:50 -0500

    Modifies date2millis to return Option[Long]

--- src/main/scala/barrett/udf/DateToMillis.scala
+++ src/main/scala/barrett/udf/DateToMillis.scala
@@ -9,32 +9,58 @@ package barrett.udf
  */
 
 
-import org.apache.pig.EvalFunc
-import org.apache.pig.data.Tuple
 import org.scala_tools.time.Imports._
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.data.DataType
+import org.joda.time.format.DateTimeParser
+import org.joda.time.format.DateTimeFormatterBuilder
+import java.lang.{Long => jLong}
+;
 
 
 
 object DateToMillis
 {
+  private def makeParser(s : String) : DateTimeParser =   DateTimeFormat.forPattern(s).getParser()
+  implicit  def date2Millis(input : String, allowedDateStringFormats : List[String] = List("MMM dd, yyyy","yyyyMMdd")) : Option[jLong] = {
 
-  implicit  def date2Millis(input : String) : Long = {
-
-    val formatter = DateTimeFormat.forPattern("MMM dd, yyyy")
+    val parsers = allowedDateStringFormats.map(s => makeParser(s)).toArray
+    var builder = new DateTimeFormatterBuilder()
+    val formatter = builder.append( null, parsers ).toFormatter();
     val richFormatter = RichDateTimeFormatter(formatter)
     val date = richFormatter.parseOption(input)
     date match
     {
-      case Some(d) =>  d.millis
-      case None => throw new IllegalArgumentException
+      case Some(d) =>  Some(d.millis)
+      case None => None
     }
 
   }
-
-
 }
 
 
-class DateToMillis extends EvalFunc[Long]{
-   def exec(input : Tuple) :Long = DateToMillis.date2Millis(input.get(0).asInstanceOf[String])
+class DateToMillis extends EvalFunc[jLong]{
+   def exec(input : Tuple) : java.lang.Long = {
+     val maybe = DateToMillis.date2Millis(input.get(0).asInstanceOf[String])
+     maybe match
+     {
+       case Some(l) =>  l
+       case None => null
+
+     }
+   }
+
+    def outspuastSchema(input : Schema) : Schema = {
+      val tuple = new Schema()
+      tuple.add(input.getField(0));
+      val name = getSchemaName(this.getClass().getName().toLowerCase(), input)
+      val fs = new Schema.FieldSchema(name,tuple,DataType.LONG)
+      val s = new Schema(fs)
+      s
+
+   }
+
 }
